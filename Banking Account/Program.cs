4dcd3b28aa308a9dad7ba9d30e133ab6dd77a498
Marius <56042670+V0lund@users.noreply.github.com>
2022-02-19 12:37:48 +0100

    testing 'push' aftepr adding couple of lines

--- Banking Account/Program.cs
+++ Banking Account/Program.cs
@@ -13,30 +13,35 @@ namespace classes
             account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
             Console.WriteLine(account.Balance);
             Console.WriteLine(account.GetAccountHistory());
+            account.MakeWithdrawal(200, DateTime.Now, "Movies&Books");
+            account.MakeWithdrawal(300, DateTime.Now, "Buying food and stuff");
+            account.MakeDeposit(account.Balance, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.GetAccountHistory());
+            Console.WriteLine($"Current balance is {account.Balance}.");
 
             // Test that the initial balances must be positive.
-            BankAccount invalidAccount;
-            try
-            {
-                invalidAccount = new BankAccount("invalid", -55);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.WriteLine("Exception caught creating account with negative balance");
-                Console.WriteLine(e.ToString());
-                return;
-            }
+            //BankAccount invalidAccount;
+            //try
+            //{
+            //    invalidAccount = new BankAccount("invalid", -55);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("Exception caught creating account with negative balance");
+            //    Console.WriteLine(e.ToString());
+            //    return;
+            //}
 
-            // Test for a negative balance.
-            try
-            {
-                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-            }
-            catch (InvalidOperationException e)
-            {
-                Console.WriteLine("Exception caught trying to overdraw");
-                Console.WriteLine(e.ToString());
-            }
+            //// Test for a negative balance.
+            //try
+            //{
+            //    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            //}
+            //catch (InvalidOperationException e)
+            //{
+            //    Console.WriteLine("Exception caught trying to overdraw");
+            //    Console.WriteLine(e.ToString());
+            //}
            
         }
     }
